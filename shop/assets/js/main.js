a7275c3892ccd2d1c1e7abe75ab1b2a92d6a9bf0
Duminicstefan <aspxx1992@gmail.com>
2023-03-04 15:27:57 +0200

    acasa

--- shop/assets/js/main.js
+++ shop/assets/js/main.js
@@ -7,6 +7,28 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   touch: true,
   ride: false,
 })
+var id;
+$("#modalForm").on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget); //Button that triggered the modal
+  const produs = button.data('name') //need to find the button and get product
+  const pret = button.data('price') //need to find the button and get price
+  const description = button.data('description')
+  $(".modal-header #titleModalLabel").html(produs);
+  $(".modal-body #price").html(pret);
+  $(".modal-body #description").html(description);
+  id = button.attr('id');
+  // var datainfo = document.getElementById(id);
+  alert(JSON.stringify(button));
+  // alert(JSON.stringify(datainfo));
+
+
+})
+$("#modalForm").on('hidden.bs.modal', function (event) {
+
+  // alert('Modalul s-a inchis');
+  document.getElementById(id).classList.remove('btn-primary');
+  document.getElementById(id).classList.add('btn-info');
+})
 
 // Get the button:
 let mybutton = document.getElementById("myBtn");
@@ -27,6 +49,11 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
 
 
 var id;
--- shop/dev/js/custom.js
+++ shop/dev/js/custom.js
@@ -17,3 +17,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
